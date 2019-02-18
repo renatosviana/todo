ba22788b5f2dfabd12b42bb3d5fcb92c3c299a73
Renato Viana <renatosviana@gmail.com>
2019-02-18 18:15:04 -0300

    Implementing Validations.

--- todo-restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/user/User.java
+++ todo-restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/user/User.java
@@ -2,12 +2,18 @@ package com.todo.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
 	
+	//TODO: It is not working in my tests. 
+	@Size(min=2, message="Name should have at least 2 characters")
 	private String name;
 	
+	@Past
 	private Date birthDate;
 	
 	protected User() {
--- todo-restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/user/UserController.java
+++ todo-restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,15 @@ public class UserController {
 		
 		return user;
 	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		
+	}	
 	
 	//input - details of user
 	//output - CREATED & Return the created URI
