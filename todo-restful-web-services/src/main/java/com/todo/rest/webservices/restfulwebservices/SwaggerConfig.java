477febad0b133c8a6c550d6266828dd937c86ca3
Renato Viana <renatosviana@gmail.com>
2019-02-18 23:23:59 -0300

    Enhancing Swagger Documentation with customs annotations.

--- todo-restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ todo-restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,8 +1,16 @@
 package com.todo.rest.webservices.restfulwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,9 +21,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	  public static final Contact DEFAULT_CONTACT = new Contact("Renato Viana", "www.todo.com", "todo@gmail.com");
+	  public static final ApiInfo DEFAULT_API_INFO = 
+			  new ApiInfo("TODO Documentation Title", 
+			  "TODO Documentation Description", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", 
+	          "http://www.apache.org/licenses/LICENSE-2.0");
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json","application/xml"));
+
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 	//Bean - Docket
 	//Swagger 2
